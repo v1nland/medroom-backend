f74f2a627d7297561a80876e57d8f0407194b9cb
m0s0f6y <martin.saavedra@walmart.com>
2021-01-05 21:37:26 -0300

    Cambio en repositorio de grupos.

--- Repositories/GrupoRepository.go
+++ Repositories/GrupoRepository.go
@@ -39,14 +39,14 @@ func GetOneGrupoEstudiante(u *Models.Grupo, id string, id_curso string, id_estud
 }
 
 func GetGruposEvaluador(u *[]Models.Grupo, id_curso string, id_evaluador string) (err error) {
-	if err := Config.DB.Session(&gorm.Session{FullSaveAssociations: true}).Preload(clause.Associations).Table("evaluadores").Select("g.*").Joins("JOIN grupos g ON g.id_evaluador = evaluadores.id").Joins("JOIN cursos c ON g.id_curso = c.id").Where("c.id = ? AND evaluadores.id = ?", id_curso, id_evaluador).First(u).Error; err != nil {
+	if err := Config.DB.Session(&gorm.Session{FullSaveAssociations: true}).Preload(clause.Associations).Table("evaluadores").Select("g.*").Joins("JOIN evaluadores_grupos eg ON eg.id_evaluador = evaluadores.id").Joins("JOIN grupos g ON g.id = eg.id_grupo").Joins("JOIN cursos c ON g.id_curso = c.id").Where("c.id = ? AND evaluadores.id = ?", id_curso, id_evaluador).First(u).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
 func GetOneGrupoEvaluador(u *Models.Grupo, id string, id_curso string, id_evaluador string) (err error) {
-	if err := Config.DB.Session(&gorm.Session{FullSaveAssociations: true}).Preload(clause.Associations).Table("evaluadores").Select("g.*").Joins("JOIN grupos g ON g.id_evaluador = evaluadores.id").Joins("JOIN cursos c ON g.id_curso = c.id").Where("g.id = ? AND c.id = ? AND evaluadores.id = ?", id, id_curso, id_evaluador).First(u).Error; err != nil {
+	if err := Config.DB.Session(&gorm.Session{FullSaveAssociations: true}).Preload(clause.Associations).Table("evaluadores").Select("g.*").Joins("JOIN evaluadores_grupos eg ON eg.id_evaluador = evaluadores.id").Joins("JOIN grupos g ON g.id = eg.id_grupo").Joins("JOIN cursos c ON g.id_curso = c.id").Where("g.id = ? AND c.id = ? AND evaluadores.id = ?", id, id_curso, id_evaluador).First(u).Error; err != nil {
 		return err
 	}
 	return nil
