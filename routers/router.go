6cdafba2b52d88948efb88e4feeda722274dd895
m0s0f6y <martin.saavedra@walmart.com>
2021-03-11 20:21:20 -0300

    change cors

--- routers/router.go
+++ routers/router.go
@@ -8,11 +8,14 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	config := cors.DefaultConfig()
-	config.AllowAllOrigins = true
-	config.AddAllowHeaders("Authorization")
-
-	r.Use(cors.New(config))
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
+	}))
 
 	r = SetupPublicRouter(r)
 	r = SetupEstudianteRouter(r)
