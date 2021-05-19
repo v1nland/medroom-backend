fea403f2eebf6172334f6709c79a3a759b69c294
m0s0f6y <martin.saavedra@walmart.com>
2021-05-19 19:29:38 -0400

    putProfile admin_academico fix

--- controllers/administrador_academico/put_profile.go
+++ controllers/administrador_academico/put_profile.go
@@ -11,48 +11,12 @@ import (
 )
 
 type putProfileRequest struct {
-	Id_rol                                        *int    `json:"id_rol"`
-	Rut_administrador_academico                   *string `json:"rut_administrador_academico"`
-	Nombres_administrador_academico               *string `json:"nombres_administrador_academico"`
-	Apellidos_administrador_academico             *string `json:"apellidos_administrador_academico"`
 	Hash_contrasena_administrador_academico       *string `json:"hash_contrasena_administrador_academico"`
 	Hash_nueva_contrasena_administrador_academico *string `json:"hash_nueva_contrasena_administrador_academico"`
-	Correo_electronico_administrador_academico    *string `json:"correo_electronico_administrador_academico"`
-	Telefono_fijo_administrador_academico         *string `json:"telefono_fijo_administrador_academico"`
 	Telefono_celular_administrador_academico      *string `json:"telefono_celular_administrador_academico"`
 }
 
 func putProfileRequestFormat(u *putProfileRequest) {
-	if u.Rut_administrador_academico != nil {
-		*u.Rut_administrador_academico = strings.TrimSpace(*u.Rut_administrador_academico)
-		*u.Rut_administrador_academico = strings.ToUpper(*u.Rut_administrador_academico)
-		*u.Rut_administrador_academico = utils.RemoveAccents(*u.Rut_administrador_academico)
-	}
-
-	if u.Nombres_administrador_academico != nil {
-		*u.Nombres_administrador_academico = strings.TrimSpace(*u.Nombres_administrador_academico)
-		*u.Nombres_administrador_academico = strings.ToUpper(*u.Nombres_administrador_academico)
-		*u.Nombres_administrador_academico = utils.RemoveAccents(*u.Nombres_administrador_academico)
-	}
-
-	if u.Apellidos_administrador_academico != nil {
-		*u.Apellidos_administrador_academico = strings.TrimSpace(*u.Apellidos_administrador_academico)
-		*u.Apellidos_administrador_academico = strings.ToUpper(*u.Apellidos_administrador_academico)
-		*u.Apellidos_administrador_academico = utils.RemoveAccents(*u.Apellidos_administrador_academico)
-	}
-
-	if u.Correo_electronico_administrador_academico != nil {
-		*u.Correo_electronico_administrador_academico = strings.TrimSpace(*u.Correo_electronico_administrador_academico)
-		*u.Correo_electronico_administrador_academico = strings.ToUpper(*u.Correo_electronico_administrador_academico)
-		*u.Correo_electronico_administrador_academico = utils.RemoveAccents(*u.Correo_electronico_administrador_academico)
-	}
-
-	if u.Telefono_fijo_administrador_academico != nil {
-		*u.Telefono_fijo_administrador_academico = strings.TrimSpace(*u.Telefono_fijo_administrador_academico)
-		*u.Telefono_fijo_administrador_academico = strings.ToUpper(*u.Telefono_fijo_administrador_academico)
-		*u.Telefono_fijo_administrador_academico = utils.RemoveAccents(*u.Telefono_fijo_administrador_academico)
-	}
-
 	if u.Telefono_celular_administrador_academico != nil {
 		*u.Telefono_celular_administrador_academico = strings.TrimSpace(*u.Telefono_celular_administrador_academico)
 		*u.Telefono_celular_administrador_academico = strings.ToUpper(*u.Telefono_celular_administrador_academico)
@@ -100,8 +64,8 @@ func PutProfile(c *gin.Context) {
 		Nombres_administrador_academico:         admin.Nombres_administrador_academico,
 		Apellidos_administrador_academico:       admin.Apellidos_administrador_academico,
 		Hash_contrasena_administrador_academico: utils.CheckNullString(input.Hash_nueva_contrasena_administrador_academico, admin.Hash_contrasena_administrador_academico),
-		Correo_electronico_administrador_academico: utils.CheckNullString(input.Correo_electronico_administrador_academico, admin.Correo_electronico_administrador_academico),
-		Telefono_fijo_administrador_academico:      utils.CheckNullString(input.Telefono_fijo_administrador_academico, admin.Telefono_fijo_administrador_academico),
+		Correo_electronico_administrador_academico: admin.Correo_electronico_administrador_academico,
+		Telefono_fijo_administrador_academico:      admin.Telefono_fijo_administrador_academico,
 		Telefono_celular_administrador_academico:   utils.CheckNullString(input.Telefono_celular_administrador_academico, admin.Telefono_celular_administrador_academico),
 	}
 
